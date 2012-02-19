a92fe1843c0696d6b5a305dab8ae376a149e338b
David Dallago <ddallago@enseirb-matmeca.fr>
2012-02-19 15:16:21 +0100

    ajout des fichiers pour le midi

---
+++ toMidi/toMidi.cpp
@@ -0,0 +1,148 @@
+#include "toLily.hpp"
+#include <thread>
+#include <boost/range/numeric.hpp>
+#include <fstream>
+#include "stdlib.h"
+namespace music{
+  
+  toLily::toLily(){}
+  toLily::~toLily(){
+    //free(data);
+  }
+
+  void toLily::remove_barline(){
+    data->end_repeat = false;
+    data->begin_repeat = false;
+  }
+
+  void
+  toLily::operator()(braille::ambiguous::score const& score){
+    int staff_count = 0;
+    //   
+    //   std::wcout << "open ok" << std::endl;
+    data = new backend;
+    data->begin_repeat = false;
+    data->end_repeat = false;
+
+   std::wcout << "\\version \"2.12.3\"" << std::endl;
+   std::wcout << "\\score{" << std::endl;
+
+    for(braille::ambiguous::part const& part: score.parts) {
+      	  std::wcout << "<<\n" << std::endl;
+      for(braille::ambiguous::staff const& staff: part)
+	{
+	  if (staff_count==0){
+	    std::wcout << "\t\n\\new Staff {\n\t\\clef treble" << std::endl;
+
+
+	  }
+	  else if (staff_count==1){
+	    std::wcout << "\t\n\\new Staff {\n\t\\clef bass" << std::endl;
+
+
+	  }
+
+	  staff_count++;
+	  current_position = zero;
+
+	  //	  for (int i = 0; i < staff.size(); i ++){
+	  	  for (int i = 0; i < 5; i ++){
+	    remove_barline();
+	    scan=false;
+	    boost::apply_visitor(*this,staff[i]);
+	    /* The goal here is to see if the next measure begin with a barline
+	     * So we will scan the measure to do so
+	     */
+	    if(i < staff.size() -1){
+	      scan = true;
+	      boost::apply_visitor(*this,staff[i+1]);
+	      scan = false;
+	    }
+	    if(data->begin_repeat && data->end_repeat){
+	      std::wcout << ">> \\bar \":|:\" " << "  %" << i << std::endl;
+	    }
+	    else if(data->begin_repeat){
+	      std::wcout << ">> \\bar \"|:\" " << "  %" << i<< std::endl;
+	    }
+	    else if(data->end_repeat){
+	      std::wcout << ">> \\bar \":|\""<< "  %" << i << std::endl;
+	    }
+	    else {
+	      std::wcout << ">> |" << "  %" << i << std::endl;
+	    }
+	    
+	  }
+
+	  std::wcout << "}\n" << std::endl;
+	}
+      std::wcout << "\n>>\n" << std::endl;
+    }
+    std::wcout << "}" << std::endl;
+    std::wcout << "}" << std::endl;
+  }
+
+
+  void
+  toLily::operator()(braille::ambiguous::measure const& measure){
+    if(scan){
+      for(braille::ambiguous::voice const& voice: measure.voices) {
+	for(braille::ambiguous::partial_measure const& partial_measure: voice) {
+	  for(braille::ambiguous::partial_voice const& partial_voice: partial_measure) {
+	    text_performer perform(data,scan);
+	    std::for_each(partial_voice.begin(), partial_voice.end(),boost::apply_visitor(perform));
+	  }
+	}
+      }
+    }
+    else {
+      std::wcout << "<<";
+      for(int j = 0 ; j < measure.voices.size(); j++){
+	std::wcout << "{";
+	for(int i =0; i < measure.voices[j].size() ; i++){
+	  for(int k = 0; k < (measure.voices[j])[i].size(); k++){
+	    // true if there is a new partial voices
+	    if( k < ((measure.voices[j])[i]).size()-1  && 1 != ((measure.voices[j])[i]).size() ){
+	      //if(1 != ((measure.voices[j])[i]).size()){
+	      std::wcout << "<<";
+	    }
+	    text_performer perform(data,false);
+	    for(int l = 0; l < (((measure.voices[j])[i])[k]).size() ; l++ ){
+	      struct backend * fakedata = new backend;
+	      boost::apply_visitor(text_performer(fakedata,true),(((measure.voices[j])[i])[k])[l]);
+	      // if( fakedata->fakescan && k < ((measure.voices[j])[i]).size()-1  && 1 != ((measure.voices[j])[i]).size() ) {
+		if(fakedata->fakescan && 1 != ((measure.voices[j])[i]).size() ){
+		  std::wcout << "{";
+	      } 
+	      boost::apply_visitor(perform,(((measure.voices[j])[i])[k])[l]);
+	      //if(fakedata->fakescan && k < ((measure.voices[j])[i]).size()-1  && 1 != ((measure.voices[j])[i]).size()){
+	      if(fakedata->fakescan && 1 != ((measure.voices[j])[i]).size() ){
+		std::wcout << "}";
+	      }
+	    }
+	    if( k == ((measure.voices[j])[i]).size()-1 && 1 != ((measure.voices[j])[i]).size() ){
+	      //if(1 != ((measure.voices[j])[i]).size() ){
+	      std::wcout << ">>";
+	    }
+	  }
+	}
+	std::wcout << "}";
+	if(j!=measure.voices.size()-1)
+	  std::wcout << "\\\\";    
+      }
+    }
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ toMidi/toMidi.hpp
@@ -0,0 +1,309 @@
+/*
+ * Braille -> Text
+ *
+ */
+
+#include <fstream>
+#include "ambiguous.hpp"
+#include <string>
+#include <iostream>
+//#include "midi.hpp"
+
+namespace music {
+  
+  struct backend {
+    // Repeat
+    bool begin_repeat;
+    bool end_repeat;
+    // FakeScan
+    bool fakescan;
+    // Octave
+    int last_note_step;
+
+  };
+
+
+class text_performer : public boost::static_visitor<void>
+
+{
+
+private:
+
+  public:
+  struct backend * data;
+  /* If scan is true we will do something different */
+  bool scan;
+  text_performer(struct backend * _data, bool _scan)//, std::ofstream &theFileB){
+  { 
+    scan = _scan;
+    data = _data;
+    data->fakescan = true;
+  }
+  ~text_performer(){}
+  
+    
+    //note management
+    
+    result_type 
+    operator()(braille::ambiguous::note const& note)const{
+	
+
+      
+      if(!scan){
+	
+	std::string s_octave = "";
+	if( (int) note.octave == 5){
+	  s_octave += "'";
+	}
+	else if( (int) note.octave == 6){
+	  s_octave += "''";
+	}
+	
+	else if ( (int) note.octave == 3){
+	  s_octave += ",";
+	}
+	
+     
+      
+      switch(note.step)
+	{
+	case 0:
+	  std::wcout<< " c" << s_octave.c_str()  << note.as_rational().denominator();
+	  break;
+	case 1:
+	  std::wcout<< " d" << s_octave.c_str() << note.as_rational().denominator();
+	  break;
+	case 2:
+	  std::wcout<< " e" << s_octave.c_str() << note.as_rational().denominator();
+	  break;
+	case 3:
+	  std::wcout<< " f" << s_octave.c_str() << note.as_rational().denominator();
+	  break;
+	case 4:
+	  std::wcout<< " g" << s_octave.c_str() << note.as_rational().denominator();
+	  break;
+	case 5:
+	  std::wcout<< " a" << s_octave.c_str() << note.as_rational().denominator();
+	  break;
+	case 6:
+	  std::wcout<< " b" << s_octave.c_str() << note.as_rational().denominator();
+	  break;
+	default:
+	  std::wcout<< " ?" ;
+	  break;
+ 	}
+    //   std::wcout << note.octave;
+          
+    //   if(note.acc){
+    // 	switch(*note.acc){
+    // 	case 0:
+    // 	  std::wcout << " Natural ";
+    // 	  break;
+    // 	case 1:
+    // 	  std::wcout << " flat ";
+    // 	  break;
+    // 	case 2:
+    // 	  std::wcout << " double_flat ";
+    // 	  break;
+    // 	case 3:
+    // 	  std::wcout << " triple_flat ";
+    // 	  break;
+    // 	case 4:
+    // 	  std::wcout << " sharp ";
+    // 	  break;
+    // 	case 5:
+    // 	  std::wcout << " double_sharp ";
+    // 	  break;
+    // 	case 6:
+    // 	  std::wcout << " triple_sharp ";
+    // 	  break;
+    // 	default:
+    // 	  break;
+    // 	}      
+    // 	}
+      }
+      
+    }
+   
+    
+    result_type
+    operator()(braille::ambiguous::barline const& barline)const{
+        switch(barline)
+	{
+	  	data->fakescan = false;
+	case 0:
+	  if(scan)
+	    data->begin_repeat = true;
+	  break;
+	case 1:
+	  if(!scan)
+	    data->end_repeat = true;
+	  break;
+	default:
+	  std::wcout << " Error barline ";
+
+	}
+	data->fakescan = false;
+    }
+
+    result_type
+    operator()(braille::ambiguous::rest const& rest)const{
+      if(!scan){
+      if(rest.whole_measure)
+	std::wcout << " r1  " ;
+      else
+      std::wcout << "  r" <</* rest.as_rational().numerator() << "/" <<*/ rest.as_rational().denominator() << "" ;
+      }
+    }
+  
+  result_type
+  operator()(braille::ambiguous::chord const& chord)const{
+    data->fakescan = false;
+    //std::wcout << "FDLKQJLDFKJQDSF" << (int) chord.base.step << (int) chord.intervals[0].steps  << std::endl;
+    if(!scan){
+      std::wcout << "<";
+      	std::string s_octave = "";
+	if( (int) chord.base.octave == 5){
+	  s_octave += "'";
+	}
+	else if( (int) chord.base.octave == 6){
+	  s_octave += "''";
+	}
+	
+	else if ( (int) chord.base.octave == 3){
+	  s_octave += ",";
+	}
+	
+      
+      switch(chord.base.step)
+	{
+	case 0:
+	  std::wcout<< " c" << s_octave.c_str(); 
+	  break;
+	case 1:
+	  std::wcout<< " d" << s_octave.c_str();
+	  break;
+	case 2:
+	  std::wcout<< " e" << s_octave.c_str();
+	  break;
+	case 3:
+	  std::wcout<< " f" << s_octave.c_str();
+	  break;
+	case 4:
+	  std::wcout<< " g" << s_octave.c_str();
+	  break;
+	case 5:
+	  std::wcout<< " a" << s_octave.c_str();
+	  break;
+	case 6:
+	  std::wcout<< " b" << s_octave.c_str() ;
+	  break;
+	default:
+	  std::wcout<< " ?" ;
+	  break;
+ 
+	}
+      
+      for(int i = 0; i < chord.intervals.size(); i++){
+	
+	int _note = chord.intervals[i].steps;
+	std::string  s_octave2 = "";
+	if(_note - chord.base.step < 0){
+	  if(s_octave == ","){
+	    s_octave2 += "";
+	  }
+	  else if (s_octave == "'"){
+	    s_octave2 += "''";
+	  }
+	  else {
+	    s_octave2 = s_octave;
+	  }
+	}
+	
+	else {
+	  s_octave2 = s_octave;
+	}
+	
+	switch((chord.base.step+_note+1)%7)
+	  {
+	    case 0:
+	      std::wcout<< " c" << s_octave2.c_str(); 
+	      break;
+	    case 1:
+	    std::wcout<< " d" << s_octave2.c_str();
+	    break;
+	    case 2:
+	    std::wcout<< " e" << s_octave2.c_str();
+	    break;
+	    case 3:
+	    std::wcout<< " f" << s_octave2.c_str();
+	    break;
+	    case 4:
+	    std::wcout<< " g" << s_octave2.c_str();
+	    break;
+	    case 5:
+	    std::wcout<< " a" << s_octave2.c_str();
+	    break;
+	    case 6:
+	    std::wcout<< " b" << s_octave2.c_str() ;
+	    break;
+	    default:
+	    std::wcout<< " ?" ;
+	    break;
+ 
+	  }
+      }
+      std::wcout << ">" << chord.as_rational().denominator();
+      
+      //    std::wcout << " %{CHORD}% " << std::endl;
+    }
+  }
+  
+  result_type
+  operator()(braille::ambiguous::value_distinction const& value_distinction) const{
+    if(!scan){
+      //std::wcout << " %{VALUE_DISTINCTION}% " << std::endl ;
+    }
+  }
+  
+  result_type
+  operator()(braille::ambiguous::hand_sign const& hand_sign) const{
+    if(!scan){
+      //std::wcout << " %{HAND_SIGN}% " << std::endl;
+    }
+  }
+
+  result_type
+  operator()(braille::ambiguous::simile const& simile) const{
+    if(!scan){
+      // std::wcout << " %{SIMILE}% " << std::endl ;
+    }
+  }
+  
+
+
+
+
+    template<typename T> result_type operator()(T const&) const {}
+  };
+
+
+  class toLily : public boost::static_visitor<void> {
+    rational current_position;
+
+  private:
+    void remove_barline();
+    /* If scan is true we will do something different */
+    bool scan;
+  public:
+    struct backend * data; 
+    toLily();
+    ~toLily();
+    void operator()(braille::ambiguous::score const&);
+    void operator()(braille::ambiguous::measure const&);
+  };
+
+
+
+
+}
